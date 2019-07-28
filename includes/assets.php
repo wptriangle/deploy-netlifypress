25c4148e3aea413608c73d728203142df479e7b6
nfmohit-wpmudev <nfmohit49@gmail.com>
2019-07-28 10:58:02 +0600

    Enqueued CSS and JS files

---
+++ includes/assets.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Enqueue and handle assets
+ *
+ * @since      1.0
+ * @package    NetlifyPress
+ * @author     Nahid Ferdous Mohit
+ */
+
+/*
+ * Admin assets
+ */
+
+function netlifypress_enqueue_admin_assets() {
+    wp_enqueue_script( 'netlifypress-admin-script',  plugin_dir_url(dirname(__FILE__)) . 'assets/dist/js/main.min.js' );
+    wp_enqueue_style( 'netlifypress-admin-style',  plugin_dir_url(dirname(__FILE__)) . 'assets/dist/css/main.min.css' );
+}
+add_action( 'admin_enqueue_scripts', 'netlifypress_enqueue_admin_assets' );
