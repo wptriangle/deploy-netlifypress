081ef55f195142fd93eda06f4f5b12bd9c06b5aa
nfmohit-wpmudev <nfmohit49@gmail.com>
2019-07-29 13:12:49 +0600

    Enqueue assets only on the plugin admin page(s)

--- includes/assets.php
+++ includes/assets.php
@@ -11,8 +11,12 @@
  * Admin assets
  */
 
-function netlifypress_enqueue_admin_assets() {
-    wp_enqueue_script( 'netlifypress-admin-script',  plugin_dir_url(dirname(__FILE__)) . 'assets/dist/js/main.min.js' );
-    wp_enqueue_style( 'netlifypress-admin-style',  plugin_dir_url(dirname(__FILE__)) . 'assets/dist/css/main.min.css' );
+function netlifypress_enqueue_admin_assets( $hook ) {
+    global $netlifypress_options_page;
+	if( $hook != $netlifypress_options_page ) {
+		return;
+	}
+    wp_enqueue_script( 'netlifypress-admin-script',  plugin_dir_url( dirname( __FILE__ ) ) . 'assets/dist/js/main.min.js' );
+    wp_enqueue_style( 'netlifypress-admin-style',  plugin_dir_url( dirname( __FILE__ ) ) . 'assets/dist/css/main.min.css' );
 }
 add_action( 'admin_enqueue_scripts', 'netlifypress_enqueue_admin_assets' );
