24993a820b0a840b04cadf5fbcc279fb8e13ef79
nfmohit-wpmudev <nfmohit49@gmail.com>
2020-03-13 12:35:45 +0600

    Deleted unnecessary comment

--- webpack.config.js
+++ webpack.config.js
@@ -13,11 +13,6 @@ const path = require( 'path' );
  */
 
 const UnminifiedWebpackPlugin = require( 'unminified-webpack-plugin' );
-
-/*
- * Plugins
- */
-
 const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
 
 /*
